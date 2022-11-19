818d12f82918d4233905360ea78ecea3044d0e04
WigglyDonnie <donfather1000@gmail.com>
2022-11-19 12:21:17 -0700

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "bingbongda9d33fd": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator, Button } from "@aws-amplify/ui-react";
+
+interface signOutInterface {
+  signOut?: Function;
+}
+
+function App({ signOut }: signOutInterface) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>Beep Boop</div>
+        <Button marginTop={25} color="white" onClick={signOut}>
+          logout
+        </Button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
