d8b82d49026aa908ea98cc1bf63c61cecebb5819
WigglyDonnie <donfather1000@gmail.com>
2022-11-19 18:58:03 -0700

    restore env

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "bingbongda9d33fd": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
     "api": {
         "bingbong": {
             "GraphQLAPIKeyOutput": "string",
