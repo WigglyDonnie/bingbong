b64870518628534911b06004b8fc42234acf0f97
WigglyDonnie <donfather1000@gmail.com>
2022-11-19 18:09:57 -0700

    add a few todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
+import logo from "./logo.svg";
+import { API } from "aws-amplify";
 import { withAuthenticator, Button } from "@aws-amplify/ui-react";
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
 
 interface signOutInterface {
   signOut?: () => {};
 }
 
 function App({ signOut }: signOutInterface) {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    // TODO fix any
+    const apiData: any = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    setNotes(notesFromAPI);
+  }
+  //TODO fix any
+  async function createNote(event: any) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+    };
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+  // TODO fix double any :(:(:(:(:()))))
+  async function deleteNote({ id }: any) {
+    const newNotes = notes.filter((note: any) => note.id !== id);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
